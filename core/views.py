3db816d4bcb99278a2c2d587c07a0bf4643aa145
Xamir Rojas <shamir0735@gmail.com>
2019-10-20 17:50:46 -0500

    redirect_home

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def test_template(request):
     return render(request, 'base.html')
+
+def redirect_home(request):
+    return HttpResponseRedirect(reverse('home'))
